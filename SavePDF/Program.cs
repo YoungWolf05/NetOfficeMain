0b665cb4de7eec6827b798d66f50ebed396fd380
Choon-Feng Sng <choonfeng.sng05@hotmail.com>
2022-09-16 10:38:17 +0800

    Example of Saving PDF using NetOffice.WordAPI

---
+++ SavePDF/Program.cs
@@ -0,0 +1,45 @@
+﻿SaveAsPDF("C:/Users/choonsng/Desktop/hello.docx");
+
+
+void SaveAsPDF(string filepath)
+{
+    if (!File.Exists(filepath.ToString()))
+    {
+        Console.WriteLine($"{filepath} not exists");
+        return;
+    }
+
+    // Create netoffice application
+    NetOffice.WordApi.Application word = new NetOffice.WordApi.Application();
+
+    // Hide the application
+    word.Visible = false;
+    word.ScreenUpdating = false;
+
+    string SourceFile = filepath ;
+    object filename = SourceFile;
+
+    // Open the document
+    NetOffice.WordApi.Document doc = word.Documents.Open(filename);
+    doc.Activate();
+
+    // Change the file extension from docx to pdf
+    object outputFileName = SourceFile.Replace(".docx", ".pdf");
+    object fileFormat = NetOffice.WordApi.Enums.WdSaveFormat.wdFormatPDF;
+
+    // Saving the file as pdf
+    if (!File.Exists(outputFileName.ToString()))
+    {
+        doc.SaveAs(outputFileName, fileFormat);
+        Console.WriteLine($"Save pdf to {outputFileName}");
+    }
+    else
+        Console.WriteLine($"{outputFileName} already exists");
+
+    // Close the document
+    object saveChanges = NetOffice.WordApi.Enums.WdSaveOptions.wdDoNotSaveChanges;
+    ((NetOffice.WordApi._Document)doc).Close(saveChanges);
+
+    // Quit the application
+    ((NetOffice.WordApi._Application)word).Quit();
+}
